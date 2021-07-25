b661cef706ad2fb5cd8e64d7ad04453a2950dbb0
Ivan <ivan.szs@hotmail.com>
2021-07-25 04:08:19 -0300

    upload clase-2 ejemplos

---
+++ clase-2/ejemplo-jsx-simple/src/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const lista = [
+  "Mean Girls",
+  "Die Hard",
+  "Hot Fuzz",
+  "Kill Bill",
+  "John Wick",
+  "Shrek",
+  "Ted",
+  "The Godfather",
+  "Avengers",
+  "Amelie",
+  "The Dark Knight",
+  "Angry Men",
+  "Flashback",
+  "Luca",
+  "Cruella",
+  "Pulp Fiction",
+  "The Good, the Bad and the Ugly",
+  "Fight Club",
+  "Forrest Gump",
+  "Inception",
+  "Goodfellas",
+  "Life Is Beautiful",
+  "Spirited Away",
+  "Interstellar",
+  "The Pianist",
+  "etc"
+];
+
+ReactDOM.render(
+  <ul>
+    <li>Mean Girls</li>
+    <li>Die Hard</li>
+    <li>Hot Fuzz</li>
+    <li>Kill Bill</li>
+    <li>John Wick</li>
+    <li>Shrek</li>
+    <li>Ted</li>
+    <li>The Godfather</li>
+    <li>Avengers</li>
+    <li>Amelie</li>
+    <li>The Dark Knight</li>
+    <li>Angry Men</li>
+    <li>Flashback</li>
+    <li>Luca</li>
+    <li>Cruella</li>
+    <li>Pulp Fiction</li>
+    <li>The Good, the Bad and the Ugly</li>
+    <li>Fight Club</li>
+    <li>Forrest Gump</li>
+    <li>Inception</li>
+    <li>Goodfellas</li>
+    <li>Life Is Beautiful</li>
+    <li>Spirited Away</li>
+    <li>Interstellar</li>
+    <li>The Pianist</li>
+    <li>etc</li>
+  </ul>,
+  document.getElementById("root")
+);
