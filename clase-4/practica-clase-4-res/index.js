78e8ef5ca6fef9b4b80209688d16f7962dfd2d24
Ivan <ivan.szs@hotmail.com>
2021-08-06 14:47:22 -0300

    Add clase-4

---
+++ clase-4/practica-clase-4-res/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import meme from "./src/satisfied.png";
+
+ReactDOM.render(
+  <img src={meme} alt="satisfied meme" />,
+  document.getElementById("root")
+);
