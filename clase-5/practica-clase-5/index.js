e6bb5460708a8be563c34c787b72f1300591bb4d
Ivan <ivan.szs@hotmail.com>
2021-08-10 14:14:04 -0300

    Add clase 5

---
+++ clase-5/practica-clase-5/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import EnsaladaDeFrutas from "./src/EnsaladaDeFrutas"
+
+ReactDOM.render(
+  <EnsaladaDeFrutas />,
+  document.getElementById("root")
+);
