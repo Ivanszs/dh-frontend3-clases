10a7e6510a46c0001e638d230eaf0fdba36694de
ivanszs <ivan.szs@hotmail.com>
2021-08-12 17:48:33 -0300

    Upload practica-clase-7 and practica-clase-7-res

---
+++ clase-7/practica-clase-7-res/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Puntos from "./components/Puntos"
+
+class App extends React.Component {
+  render() {
+    return <Puntos />
+  }
+}
+
+export default App;
---
+++ clase-7/practica-clase-7/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./src/App";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root"));
