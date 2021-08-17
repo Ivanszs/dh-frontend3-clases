7a48820a89bb2a0dd0880399c8447f426df1ddc3
ivanszs <ivan.szs@hotmail.com>
2021-08-17 16:37:21 -0300

    Add practica-clase-10-res

---
+++ clase-10/practica-clase-10-res/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+                    imHungryFor: "Empanadas", 
+                    show: true
+                 };
+  }
+
+  cancelOrder = () => {
+    this.setState({show: false});
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({imHungryFor: "Pizzas"})
+    }, 2000)
+  }
+  componentDidUpdate() {
+    console.log("El componente se actualizó!");
+  }
+  render() {
+    let myOrder;
+    if (this.state.show) {
+      myOrder = <Child food={this.state.imHungryFor}/>;
+    };
+    return (
+      <div>
+      {myOrder}
+      <button type="button" onClick={this.cancelOrder}>Cancelar pedido.</button>
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("Tu pedido ha sido cancelado.");
+  }
+  render() {
+    return (
+      <h1>Tu pedido: {this.props.food}</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Container />, document.getElementById('root'));
